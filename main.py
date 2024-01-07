c5e60bbb0b841cdec749915dacd1b5b668ca18de
zhandrov@firsov-vostro-3490 <osintfir@gmail.com>
2024-01-07 16:32:30 +0300

    make func 'get_text_after_substitution' a method of FileManager

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
 from managers import FileManager
 
 
-def get_text_after_substitution(*, text: str, subs_dict: dict) -> str:
-    return ''
-
-
 def main():
     text_filepath = FileManager.get_filepath()
     text = FileManager.get_from_txt(filepath=text_filepath)
     subs_dict = FileManager.get_from_json()
 
-    result_text = get_text_after_substitution(text=text, subs_dict=subs_dict)
+    result_text = FileManager.get_text_after_substitution(initial_text=text, subs_dict=subs_dict)
     FileManager.save_to_file(text=result_text)
 
 
